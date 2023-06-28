0cb5b4c3e08533b6464522ce87e0d310709ba46b
kleber.barilli <kleber.barilli73@gmail.com>
2023-06-28 10:54:28 -0300

    ms customers initial setup

---
+++ mscustomers/src/main/java/io/github/kleberbarilli/mscustomers/MscustomersApplication.java
@@ -0,0 +1,13 @@
+package io.github.kleberbarilli.mscustomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscustomersApplication.class, args);
+	}
+
+}
