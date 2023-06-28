a1f48c16b4359f419c0382c8cb4233fdd576949a
kleber.barilli <kleber.barilli73@gmail.com>
2023-06-28 14:25:57 -0300

    add api gateway setup

---
+++ apigateway/src/test/java/io/github/kleberbarilli/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kleberbarilli.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
