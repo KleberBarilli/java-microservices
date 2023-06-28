680137764041584ca0c406a955f79b0ce471c67e
kleber.barilli <kleber.barilli73@gmail.com>
2023-06-28 16:52:21 -0300

    add get cards from income

--- msicards/src/main/java/io/github/kleberbarilli/msicards/application/controllers/ICardsController.java
+++ msicards/src/main/java/io/github/kleberbarilli/msicards/application/controllers/ICardsController.java
@@ -1,10 +1,12 @@
 package io.github.kleberbarilli.msicards.application.controllers;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.kleberbarilli.msicards.application.domain.dtos.ICardDTO;
@@ -33,6 +35,15 @@ public class ICardsController {
         ICard card = iCardDTO.toEntity();
         service.save(card);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
+
+    @GetMapping(params = "income")
+    public ResponseEntity<List<ICard>> getICardsPerIncome(@RequestParam("income") Long income) {
+
+        List<ICard> cardList = service.getCardsFromIncome(income);
+
+        return ResponseEntity.ok(cardList);
 
     }
 
