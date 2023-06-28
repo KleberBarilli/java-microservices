3bf4c8c78c0356f88120e3fa0636ff6999f8d7dc
kleber.barilli <kleber.barilli73@gmail.com>
2023-06-28 10:24:16 -0300

    invest-x server initial setup

---
+++ investxserver/src/main/java/io/github/kleberbarilli/investxserver/InvestxserverApplication.java
@@ -0,0 +1,13 @@
+package io.github.kleberbarilli.investxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestxserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestxserverApplication.class, args);
+	}
+
+}
