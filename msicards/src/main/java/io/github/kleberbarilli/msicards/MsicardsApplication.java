35fc5b7c8c3e501840ace7d719f4b955b746997d
kleber.barilli <kleber.barilli73@gmail.com>
2023-06-28 15:11:57 -0300

    add ms icards setup

---
+++ msicards/src/main/java/io/github/kleberbarilli/msicards/MsicardsApplication.java
@@ -0,0 +1,13 @@
+package io.github.kleberbarilli.msicards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsicardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsicardsApplication.class, args);
+	}
+
+}
---
+++ msicards/src/test/java/io/github/kleberbarilli/msicards/MsicardsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kleberbarilli.msicards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsicardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
