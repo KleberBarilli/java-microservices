d9320e20845c60672c96c2d42de34abfff2c4390
kleber.barilli <kleber.barilli73@gmail.com>
2023-06-28 10:47:11 -0300

    add missing eureka annotation

--- investxserver/src/main/java/io/github/kleberbarilli/investxserver/InvestxserverApplication.java
+++ investxserver/src/main/java/io/github/kleberbarilli/investxserver/InvestxserverApplication.java
@@ -2,7 +2,9 @@ package io.github.kleberbarilli.investxserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class InvestxserverApplication {
 
