c8684bef146e52730ea5d26803382c2717b987f0
kleber.barilli <kleber.barilli73@gmail.com>
2023-06-28 14:35:46 -0300

    api gateway config

--- apigateway/src/main/java/io/github/kleberbarilli/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/io/github/kleberbarilli/apigateway/ApigatewayApplication.java
@@ -2,12 +2,25 @@ package io.github.kleberbarilli.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(r -> r.path("/customers/**").uri("lb://mscustomers"))
+				.build();
 	}
 
 }
