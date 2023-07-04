ced2f5efb6eb75d266a2b0596c545e35e8307f2b
kleber.barilli <kleber.barilli73@gmail.com>
2023-07-04 14:57:15 -0300

    wip

--- mscreditappraiser/src/main/java/io/github/kleberbarilli/mscreditappraiser/application/services/CreditAppraiserService.java
+++ mscreditappraiser/src/main/java/io/github/kleberbarilli/mscreditappraiser/application/services/CreditAppraiserService.java
@@ -1,10 +1,14 @@
 package io.github.kleberbarilli.mscreditappraiser.application.services;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import io.github.kleberbarilli.mscreditappraiser.application.domain.CustomerCard;
 import io.github.kleberbarilli.mscreditappraiser.application.domain.CustomerData;
 import io.github.kleberbarilli.mscreditappraiser.application.domain.CustomerStatus;
+import io.github.kleberbarilli.mscreditappraiser.infra.clients.CardsResourceClient;
 import io.github.kleberbarilli.mscreditappraiser.infra.clients.CustomerResourceClient;
 import lombok.RequiredArgsConstructor;
 
@@ -13,11 +17,16 @@ import lombok.RequiredArgsConstructor;
 public class CreditAppraiserService {
 
     private final CustomerResourceClient customersClient;
+    private final CardsResourceClient cardsClient;
 
     public CustomerStatus getCustomerStatus(String cpf) {
         ResponseEntity<CustomerData> customerResponse = customersClient.getCustomerData(cpf);
+        ResponseEntity<List<CustomerCard>> cardsResponse = cardsClient.getCustomerICards(cpf);
 
-        return CustomerStatus.builder().customer(customerResponse.getBody()).build();
+        return CustomerStatus.builder()
+                .customer(customerResponse.getBody())
+                .cards(cardsResponse.getBody())
+                .build();
     }
 
 }
---
+++ mscreditappraiser/src/main/java/io/github/kleberbarilli/mscreditappraiser/infra/clients/CardsResourceClient.java
@@ -0,0 +1,17 @@
+package io.github.kleberbarilli.mscreditappraiser.infra.clients;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io.github.kleberbarilli.mscreditappraiser.application.domain.CustomerCard;
+
+@FeignClient(value = "msicards", path = "/icards")
+public interface CardsResourceClient {
+
+    @GetMapping(params = "cpf")
+    ResponseEntity<List<CustomerCard>> getCustomerICards(@RequestParam("cpf") String cpf);
+}
