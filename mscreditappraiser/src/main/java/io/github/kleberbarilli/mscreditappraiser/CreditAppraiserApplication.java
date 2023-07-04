c276a2800fb4ef3b370fe630264ecb8bf5dc7f05
kleber.barilli <kleber.barilli73@gmail.com>
2023-07-04 13:30:12 -0300

    add ms credit appraiser

---
+++ mscreditappraiser/src/main/java/io/github/kleberbarilli/mscreditappraiser/CreditAppraiserApplication.java
@@ -0,0 +1,13 @@
+package io.github.kleberbarilli.mscreditappraiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAppraiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditAppraiserApplication.class, args);
+	}
+
+}
---
+++ mscreditappraiser/src/test/java/io/github/kleberbarilli/mscreditappraiser/CreditAppraiserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kleberbarilli.mscreditappraiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAppraiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
