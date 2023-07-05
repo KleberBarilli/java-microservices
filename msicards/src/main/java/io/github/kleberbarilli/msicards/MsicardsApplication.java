8d920152465b835a41f75d4cb01bd939179a6882
kleber.barilli <kleber.barilli73@gmail.com>
2023-07-05 14:31:37 -0300

    msi cards enable rabbit mq

--- msicards/src/main/java/io/github/kleberbarilli/msicards/MsicardsApplication.java
+++ msicards/src/main/java/io/github/kleberbarilli/msicards/MsicardsApplication.java
@@ -1,11 +1,13 @@
 package io.github.kleberbarilli.msicards;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableRabbit
 public class MsicardsApplication {
 
 	public static void main(String[] args) {
